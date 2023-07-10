dbe46f6999c5697545e75cbf06a1b7eb6ac971cb
Rayan Kazi <rayankazi7515@gmail.com>
2023-07-09 20:45:46 -0700

    made callout dark mode friendly

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -17,8 +17,9 @@ export default function Callout({
     <div
       className={twMerge(
         clsx("my-6 flex items-start rounded-md border border-l-4 p-4", {
-          "border-red-900 bg-red-50": type === "danger",
-          "border-yellow-900 bg-yellow-50": type === "warning",
+          "border-red-900 bg-red-50 dark:bg-red-800": type === "danger",
+          "border-yellow-900 bg-yellow-50 dark:bg-yellow-800":
+            type === "warning",
         })
       )}
       {...props}
